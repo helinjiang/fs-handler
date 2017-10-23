b1e4424dc9e905800e018f6de7b60ab574ee1bbb
helinjiang <me@helinjiang.com>
2017-10-23 23:58:40 +0800

    feat: 取消 fse 模块，目前看来不需要

--- src/fse/index.js
+++
@@ -1,4 +0,0 @@
-const fse = require('fs-extra');
-const Promise = require('bluebird');
-
-module.exports = Promise.promisifyAll(fse);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const fse = require('./fse');
 const search = require('./search');
 const util = require('./util');
 
 module.exports = {
-  fse: fse,
   search: search,
   util: util
 };
