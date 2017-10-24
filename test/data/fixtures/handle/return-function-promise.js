e07a0a53c03652ef3523fbd61ce18f60786a789f
helinjiang <me@helinjiang.com>
2017-10-24 09:31:29 +0800

    feat: 修改文件格式

--- test/data/fixtures/handle/return-function-promise.js
+++ test/data/fixtures/handle/return-function-promise.js
@@ -1,8 +1,6 @@
-import Promise from 'bluebird';
-
 export default function getData() {
-    return Promise.resolve({
-        name: 'return-function-promise',
-        age: 16
-    });
+  return Promise.resolve({
+    name: 'return-function-promise',
+    age: 16
+  });
 }
